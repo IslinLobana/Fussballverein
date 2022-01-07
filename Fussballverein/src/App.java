5387ec23fbc970fa0ae3ca04ad6f9041b671f037
Lito415 <90443897+Lito415@users.noreply.github.com>
2022-01-07 08:58:59 +0100

    Update App.java

--- Fussballverein/src/App.java
+++ Fussballverein/src/App.java
@@ -1,8 +1,7 @@
 public class App {
-    //halloajodiasd 
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
-// Das hier ist ein Kommentar
-//bruder was geht meine kliener freund der gesellschaft
+
