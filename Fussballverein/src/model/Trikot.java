2e6f546eac6eb4b104a6a5397c3185d7028ed35b
IslinLobana <90444044+IslinLobana@users.noreply.github.com>
2022-01-12 10:13:10 +0100

    trikot

--- Fussballverein/src/model/Trikot.java
+++ Fussballverein/src/model/Trikot.java
@@ -5,5 +5,29 @@ public class Trikot extends Person{
     int rueckennummer;
     String groesse;
 
+
+    public Trikot(int rueckennummer, String groesse, String vorname, String nachname){
+        super(vorname, nachname);
+        setRueckennummer(rueckennummer);
+        setGroesse(groesse);
+    }
+
+
+    public void setGroesse(String groesse) {
+        this.groesse = groesse;
+    }
+
+    public void setRueckennummer(int rueckennummer) {
+        this.rueckennummer = rueckennummer;
+    }
+
+    public String getGroesse() {
+        return groesse;
+    }
+
+    public int getRueckennummer() {
+        return rueckennummer;
+    }
+
     
 }
