ba6b6284bf992ed214df50e8d3654fd7b4a407aa
IslinLobana <90444044+IslinLobana@users.noreply.github.com>
2022-01-12 10:06:32 +0100

    ß

--- Fussballverein/src/model/Fans.java
+++ Fussballverein/src/model/Fans.java
@@ -2,6 +2,26 @@ package model;
 
 public class Fans extends Person{
 
+    String eigenschaften;
+
+
+
+
+    public Fans(String eigenschaften, String vorname, String nachname){
+        super(vorname, nachname);
+        setEigenschaften(eigenschaften);
+
+
+    }
+
+
+    public void setEigenschaften(String eigenschaften) {
+        this.eigenschaften = eigenschaften;
+    }
+
+    public String getEigenschaften() {
+        return eigenschaften;
+    }
 
     
 }
