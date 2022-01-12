c8bcc1579e56b1c51b3d2cf8348cd716cc37f540
Alexander McLoughlin <50146099+arlelm@users.noreply.github.com>
2022-01-12 10:39:06 +0100

    a

--- Fussballverein/src/controller/MainController.java
+++ Fussballverein/src/controller/MainController.java
@@ -1,6 +1,18 @@
 package controller;
 
+import model.Trikot;
+
 public class MainController {
     
+    FansController fc;
+    LigaController lc,
+    SpielerController sc,
+    SponsorenController spc, 
+    StadionController stc,
+    TicketController tc,
+    TrainerController trc, 
+    TrikotController tric, 
+    
+
     
 }
--- Fussballverein/src/model/Liga.java
+++ Fussballverein/src/model/Liga.java
@@ -7,4 +7,40 @@ public class Liga {
     String name;
     String land;
 
+
+    public Liga(int anzahlVerein, int platzierung, String name, String land){
+        setAnzahlVerein(anzahlVerein);
+        setLand(land);
+        setName(name);
+        setPlatzierung(platzierung);
+    }
+
+
+
+    public void setAnzahlVerein(int anzahlVerein) {
+        this.anzahlVerein = anzahlVerein;
+    }
+    public void setLand(String land) {
+        this.land = land;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPlatzierung(int platzierung) {
+        this.platzierung = platzierung;
+    }
+
+    public int getAnzahlVerein() {
+        return anzahlVerein;
+    }
+    public String getLand() {
+        return land;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPlatzierung() {
+        return platzierung;
+    }
+
 }
