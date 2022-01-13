368d8eb01722fe2c5938d49a6ef71fad26c69b41
IslinLobana <90444044+IslinLobana@users.noreply.github.com>
2022-01-13 18:55:17 +0100

    a

--- Fussballverein/src/controller/SpielerController.java
+++ Fussballverein/src/controller/SpielerController.java
@@ -9,20 +9,20 @@ public class SpielerController {
 
     public SpielerController(MainController mc){
         setMc(mc);
-        Spielerar = new ArrayList<Spieler>();
+        spielerar = new ArrayList<Spieler>();
     }
 
 
     public void createSpielerar(){
-        getSpielerar().add(new Spieler(20, 100000, "Jini","Wienaldüm" ));
+        getSpielerar().add(new Spieler(20, 100000, "Jini", "Wienaldüm", "Mittelfeld" true, "PSG" ));
     }
 
-    public void setSpielerar(ArrayList<Spieler> Spielerar) {
-        this.Spielerar = Spielerar;
+    public void setSpielerar(ArrayList<Spieler> spielerar) {
+        this.spielerar = spielerar;
     }
 
     public ArrayList<Spieler> getSpielerar() {
-        return Spielerar;
+        return spielerar;
     }
 
 
