61d048f96553ae880bc263036d3fc502e345370b
IslinLobana <90444044+IslinLobana@users.noreply.github.com>
2022-01-14 17:57:05 +0100

    a

--- Fussballverein/src/controller/MainController.java
+++ Fussballverein/src/controller/MainController.java
@@ -33,8 +33,8 @@ public class MainController {
     }
 
     public void createDemoData(){
-        getFc().createFans();
-        getLc().createLiga();
+        getFc().createFansar();
+        getLc().createDemoLiga();
         getSc().createSpieler();
         getSpc().createSponsoren();
         setStc().createStadion();
