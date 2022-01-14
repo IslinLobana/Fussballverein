610b77804118dd320e0f04fab92b880039698bff
IslinLobana <90444044+IslinLobana@users.noreply.github.com>
2022-01-14 08:45:37 +0100

    Trikot

---
+++ Fussballverein/src/controller/TrikotController.java
@@ -0,0 +1,32 @@
+package controller;
+import java.util.*;
+import model.Trikot;
+
+public class TrikotController {
+
+    MainController mc; 
+    ArrayList<Trikot> trikotar;
+
+    public TrikotController(MainController mc){
+        setMc(mc);
+        trikotar = new ArrayList<Trikot>();
+    }
+
+
+    public void createTrikotar(){
+        getTrikotar().add(new Trikot(9, "XXL", "Sonny", "Kittel" ));
+    }
+
+    public void setMc(MainController mc) {
+        this.mc = mc;
+    }
+    public void setTrikotar(ArrayList<Trikot> trikotar) {
+        this.trikotar = trikotar;
+    }
+    public MainController getMc() {
+        return mc;
+    }
+    public ArrayList<Trikot> getTrikotar() {
+        return trikotar;
+    }
+}    
