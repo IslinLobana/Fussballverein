3bc9cfb2de765bfc50d12abeef3f9d48b47d43f4
IslinLobana <90444044+IslinLobana@users.noreply.github.com>
2022-01-27 19:28:45 +0100

    a

--- Fussballverein/src/view/Output.java
+++ Fussballverein/src/view/Output.java
@@ -1,7 +1,11 @@
 package view;
 
 public class Output {
+    //Ausgabemethode: Durch Parameterübergabe werden übergebene Werte (data) ausgegeben--> View
     public void printData(String data){
         System.out.println(data);
+    }
+    public void printDataInt(int dataint){
+        System.out.println(dataint);
     }
 }
